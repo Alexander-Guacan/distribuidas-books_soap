142858b2bbf9723a64b262d6bdbde41130b8fcea
Alexander-Guacan <adguacan@espe.edu.ec>
2024-11-24 16:56:09 -0500

    Add python client

---
+++ client/PythonClient/Client.py
@@ -0,0 +1,13 @@
+from zeep import Client
+
+url = "http://localhost:8081/BookSoap?wsdl"
+client = Client(url)
+
+# Crear un libro
+new_book = {"Title": "Libro de Prueba", "Author": "Autor Prueba", "Year": 2024}
+response = client.service.CreateBook(newBook=new_book)
+print("Libro creado:", response)
+
+# Obtener todos los libros
+books = client.service.GetBooks()
+print("Lista de libros:", books)
